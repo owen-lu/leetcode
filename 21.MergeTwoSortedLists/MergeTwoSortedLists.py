9a88322d0ba7f5db0f270c1b254c7d19cf4eab32
Owen <Owen_Lu@htc.com>
2020-04-11 03:06:06 +0800

    improve the logic of 21.

--- 21.MergeTwoSortedLists/MergeTwoSortedLists.py
+++ 21.MergeTwoSortedLists/MergeTwoSortedLists.py
@@ -3,43 +3,36 @@ class ListNode:
         self.val = x
         self.next = None
 
+
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        if l2 is None: return l1
-        if l1 is None: return l2
         ptr1 = l1
-        ptr2 = lastRound = l2
-        prePtr2 = None
-        while True:
-            if ptr1 is None: break
-            while True:
-                if ptr1.val <= ptr2.val:    
-                    tmp = ptr1.next
-                    ptr1.next = ptr2
-                    if prePtr2 is None:
-                        l2 = ptr1
-                        prePtr2 = ptr1
-                    else:
-                        prePtr2.next = ptr1
-                        prePtr2 = ptr1
-                    ptr1 = tmp                        
-                    lastRound = ptr2
-                    break
+        ptr2 = l2
+        prePtr1 = None
+        while ptr1 is not None or ptr2 is not None:
+            if l1 is None: return l2
+            if l2 is None: return l1
+            
+            if ptr1 is None:
+                prePtr1.next = ptr2
+                ptr2 = ptr2.next
+                
+            if ptr2 is None:
+                return l1
+            
+            if ptr1.val <= ptr2.val:
+                prePtr1 = ptr1
+                ptr1 = ptr1.next                
+            else:
+                if prePtr1 is None:
+                    l1 = ptr2
+                    ptr2 = ptr2.next
+                    l1.next = ptr1
+                    prePtr1 = l1
                 else:
-                    if ptr2.next is None:
-                        ptr2.next = ptr1
-                        return l2
-                    else:
-                        if ptr1.val > ptr2.next.val:
-                            prePtr2 = ptr2
-                            ptr2 = ptr2.next
-                            continue
-                        else:
-                            tmp = ptr1.next
-                            ptr1.next = ptr2.next
-                            ptr2.next = ptr1
-                            ptr1 = tmp
-                            lastRound = ptr2.next.next
-                            break
-                ptr2 = lastRound    
-        return l2
+                    prePtr1.next = ptr2
+                    ptr2 = ptr2.next
+                    prePtr1.next.next = ptr1
+                    prePtr1 = prePtr1.next
+        return l1
+
