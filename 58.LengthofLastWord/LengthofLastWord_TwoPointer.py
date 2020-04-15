15d38e02a5a78e613aa8f2ea937619130dbaed18
Owen Lu <Owen_Lu@htc.com>
2020-04-15 18:51:11 +0800

    58. Length of Last Word
    strpe some unnecessary steps

--- 58.LengthofLastWord/LengthofLastWord_TwoPointer.py
+++ 58.LengthofLastWord/LengthofLastWord_TwoPointer.py
@@ -3,16 +3,14 @@ class Solution:
         ptr1 = len(s) - 1
         ptr2 = 0
         while ptr1 >= 0:
-            if s[ptr1] == " ":
-                ptr1 -= 1
-            else:
+            if s[ptr1] != " ":
                 break
+            ptr1 -= 1
         ptr2 = ptr1 - 1
         while ptr2 >=0:
-            if s[ptr2] != " ":
-                ptr2 -= 1
-            else:
+            if s[ptr2] == " ":
                 break
+            ptr2 -= 1
         if ptr1 < 0:
             return 0
         else:
