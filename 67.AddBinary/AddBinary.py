bedd275c02411570d0ccdc9995b9ce2e4101ebe1
Owen Lu <Owen_Lu@htc.com>
2020-04-16 15:11:19 +0800

    67. Add Binary
    revise the logic

--- 67.AddBinary/AddBinary.py
+++ 67.AddBinary/AddBinary.py
@@ -7,41 +7,40 @@ class Solution:
         ptra = len(a) - 1
         ptrb = len(b) - 1
         result = ""
-        overflow = False
+        sum_ = ""
+        preOverflow = False
         while ptra >= 0 and ptrb >= 0:
-            if b[ptrb] == "0" and overflow == False:
-                result = a[ptra] + result
-            elif b[ptrb] == "0" and overflow == True:
-                if a[ptra] == "0":
-                    result = "1" + result
-                    overflow = False
+            currentOverflow = False
+            if b[ptrb] == "0" and a[ptra] == "0":
+                sum_ = "0"
+            elif b[ptrb] == "0" and a[ptra] == "1":
+                sum_ = "1"
+            elif b[ptrb] == "1" and a[ptra] == "0":
+                sum_ = "1"
+            elif b[ptrb] == "1" and a[ptra] == "1":
+                sum_ = "0"
+                currentOverflow = True
+            if preOverflow :
+                if sum_ == "0":
+                    sum_ = "1"
                 else:
-                    result = "0" + result
-            elif b[ptrb] == "1" and overflow == False:
-                if a[ptra] == "0":
-                    result = "1" + result
-                else:
-                    result = "0" + result
-                    overflow = True
-            elif b[ptrb] == "1" and overflow == True:
-                if a[ptra] == "0":
-                    result = "0" + result
-                else:
-                    result = "1" + result
+                    sum_ = "0"
+                    currentOverflow = True
+            preOverflow = currentOverflow
+            result = sum_ + result
             ptra -= 1
             ptrb -= 1
-        while ptra >= 0:
-            if overflow == False:
+        while ptra >= 0 :
+            if preOverflow:
+                if a[ptra] == "0":
+                    result = "1" + result
+                    preOverflow = False
+                else:
+                    result = "0" + result
+            else:
                 result = a[ptra] + result
-                ptra -= 1
-                continue
-            if a[ptra] == "1":
-                result = "0" + result
-                ptra -= 1
-            if a[ptra] == "0":
-                result = "1" + result
-                ptra -= 1
-                overflow = False
-        if overflow:
-            result = "1"+ result
+            ptra -= 1
+        if preOverflow:
+            result = "1" + result
         return result
+      
