912b0fb89792805c2aa3ef4c12ef461e940c124d
Owen Lu <Owen_Lu@htc.com>
2020-04-16 18:53:42 +0800

    21. Merge Two Sorted Lists
    rewrite.

--- 21.MergeTwoSortedLists/MergeTwoSortedLists.py
+++ 21.MergeTwoSortedLists/MergeTwoSortedLists.py
@@ -1,38 +1,17 @@
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
-
-
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        ptr1 = l1
-        ptr2 = l2
-        prePtr1 = None
-        while ptr1 is not None or ptr2 is not None:
-            if l1 is None: return l2
-            if l2 is None: return l1
-            
-            if ptr1 is None:
-                prePtr1.next = ptr2
-                ptr2 = ptr2.next
-                
-            if ptr2 is None:
-                return l1
-            
-            if ptr1.val <= ptr2.val:
-                prePtr1 = ptr1
-                ptr1 = ptr1.next                
+        dummyHead = ListNode(0)
+        endOfSortedList = dummyHead
+        while l1 is not None and l2 is not None:
+            if l1.val <= l2.val:
+                endOfSortedList.next = l1
+                l1 = l1.next
             else:
-                if prePtr1 is None:
-                    l1 = ptr2
-                    ptr2 = ptr2.next
-                    l1.next = ptr1
-                    prePtr1 = l1
-                else:
-                    prePtr1.next = ptr2
-                    ptr2 = ptr2.next
-                    prePtr1.next.next = ptr1
-                    prePtr1 = prePtr1.next
-        return l1
-
+                endOfSortedList.next = l2
+                l2 = l2.next
+            endOfSortedList = endOfSortedList.next
+        if l1 is not None:
+            endOfSortedList.next = l1
+        if l2 is not None:
+            endOfSortedList.next = l2
+        return dummyHead.next
