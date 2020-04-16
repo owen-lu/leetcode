c5e10d3ffa89529f83f853ab63ae5d941190df74
Owen Lu <Owen_Lu@htc.com>
2020-04-16 19:06:52 +0800

    35. Search Insert Position
    rename variables.

--- 35.SearchInsertPosition/SearchInsertPosition.py
+++ 35.SearchInsertPosition/SearchInsertPosition.py
@@ -1,13 +1,13 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         return self.binSearch(nums, target, 0, len(nums)-1)
- 
-    def binSearch(self, nums: List[int], target: int, start: int, end: int) -> int:
-        if end < start: return start      
-        middle = int((start + end) / 2)
+
+    def binSearch(self, nums: List[int], target: int, left: int, right: int) -> int:
+        if right < left: return left
+        middle = int((left + right) / 2)
         if target == nums[middle]:
             return middle
         elif target > nums[middle]:
-            return self.binSearch(nums, target, middle+1, end)
+            return self.binSearch(nums, target, middle+1, right)
         elif target < nums[middle]:
-            return self.binSearch(nums, target, start, middle-1)
+            return self.binSearch(nums, target, left, middle-1)
