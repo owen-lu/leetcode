b6362e032823514d1aec6b6ab2de6b195544e91a
Owen Lu <Owen_Lu@htc.com>
2020-04-16 16:28:48 +0800

    67. Add Binary
    Convert to integer then calculate.

--- 67.AddBinary/AddBinary.py
+++ 67.AddBinary/AddBinary.py
@@ -1,46 +1,17 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        if len(a) < len(b):
-            tmp = a
-            a = b
-            b = tmp
         ptra = len(a) - 1
         ptrb = len(b) - 1
-        result = ""
-        sum_ = ""
-        preOverflow = False
-        while ptra >= 0 and ptrb >= 0:
-            currentOverflow = False
-            if b[ptrb] == "0" and a[ptra] == "0":
-                sum_ = "0"
-            elif b[ptrb] == "0" and a[ptra] == "1":
-                sum_ = "1"
-            elif b[ptrb] == "1" and a[ptra] == "0":
-                sum_ = "1"
-            elif b[ptrb] == "1" and a[ptra] == "1":
-                sum_ = "0"
-                currentOverflow = True
-            if preOverflow :
-                if sum_ == "0":
-                    sum_ = "1"
-                else:
-                    sum_ = "0"
-                    currentOverflow = True
-            preOverflow = currentOverflow
-            result = sum_ + result
-            ptra -= 1
-            ptrb -= 1
-        while ptra >= 0 :
-            if preOverflow:
-                if a[ptra] == "0":
-                    result = "1" + result
-                    preOverflow = False
-                else:
-                    result = "0" + result
-            else:
-                result = a[ptra] + result
-            ptra -= 1
-        if preOverflow:
-            result = "1" + result
-        return result
+        c = 0
+        s = ""
+        while ptra >= 0 or ptrb >= 0 or c == 1:
+            if ptra >= 0:
+                c = c + int(a[ptra])
+                ptra = ptra - 1
+            if ptrb >= 0:
+                c = c + int(b[ptrb])
+                ptrb = ptrb - 1
+            s = str(c % 2) + s
+            c = int(c / 2)
+        return s
       
