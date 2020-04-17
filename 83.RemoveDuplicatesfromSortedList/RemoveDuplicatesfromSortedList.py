cb673cc9c8346d3317412b5c1b2ec0c7b2d67c02
Owen Lu <Owen_Lu@htc.com>
2020-04-17 11:39:01 +0800

    83. Remove Duplicates from Sorted List
    don't need dummyHead

--- 83.RemoveDuplicatesfromSortedList/RemoveDuplicatesfromSortedList.py
+++ 83.RemoveDuplicatesfromSortedList/RemoveDuplicatesfromSortedList.py
@@ -1,15 +1,9 @@
 class Solution:
     def deleteDuplicates(self, head: ListNode) -> ListNode:
-        dummyHead = ListNode(0)
-        endOfSortedList = dummyHead
-        pre = None
-        while head is not None:
-            if pre is None or (pre.val != head.val):
-                endOfSortedList.next = head
-                endOfSortedList = endOfSortedList.next
-                pre = endOfSortedList
+        cur = head
+        while cur is not None and cur.next is not None:
+            if cur.val == cur.next.val:
+                cur.next = cur.next.next
             else:
-                pre = head
-            head = head.next
-        endOfSortedList.next = None
-        return dummyHead.next
+                cur = cur.next
+        return head
