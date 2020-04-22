d65d1a7a1a2beabebfa41f48edb55cb20cdde4a0
Owen Lu <Owen_Lu@htc.com>
2020-04-22 14:05:10 +0800

    9. Palindrome Number Don't convert integer to string

--- 9.PalindromNumber/PalindromNumber.py
+++ 9.PalindromNumber/PalindromNumber.py
@@ -1,5 +1,15 @@
+import math
+
 class Solution:
-    def isPalindrome(self, x: int) -> bool:  
-        if str(x) == str(x)[::-1]:
-            return True
-        return False
+    def isPalindrome(self, x: int) -> bool:
+        if x <= 0:
+            return x == 0
+        mask = int(math.log10(x))
+        middle = int(mask / 2)
+        mask = 10 ** mask
+        for index in range(0, middle+1):
+            if int(x / mask) != int(x % 10):
+                return False
+            x = x % mask / 10
+            mask = mask / 100
+        return True
