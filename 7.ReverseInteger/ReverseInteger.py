53d744caed287b7dda4497f3622820cbbdb9427d
Owen Lu <Owen_Lu@htc.com>
2020-04-23 16:25:23 +0800

    7. Reverse Integer
    pop and push integer

--- 7.ReverseInteger/ReverseInteger.py
+++ 7.ReverseInteger/ReverseInteger.py
@@ -1,28 +1,17 @@
 class Solution:
     def reverse(self, x: int) -> int:
         negative = False
-        if x == 0:
-            return 0
         if x < 0:
             negative = True
             x = abs(x)
-
-        digits = floor(math.log10(x))
-        mid = floor(digits / 2)
-        mask  = 10 ** digits
-        output = 0
-        for index in range(0, mid+1):
-            if index != digits-index:
-                output += int(x / mask) * (10**index)
-                output += int(x % 10) * (10**(digits-index))
-            else:
-                output += x * (10**index)
-            x = int(x % mask / 10)
-            mask = int(mask / 100)
-
+        recv = 0
+        while x != 0:
+            pop = int(x % 10)
+            recv = recv * 10 + pop
+            x = int(x / 10)
         if negative:
-            output = -output
-        if output > 2**31-1 or output < -2**31:
-            output = 0
-        return output
+            recv = -recv
+        if recv > 2**31-1 or recv < -2**31:
+            return 0
+        return recv
         
