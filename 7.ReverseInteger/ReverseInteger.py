85526ddf934e1d2bce3915e888eed20d30d412ee
Owen Lu <Owen_Lu@htc.com>
2020-04-23 15:40:33 +0800

    7. Reverse Integer
    don't convert to string

--- 7.ReverseInteger/ReverseInteger.py
+++ 7.ReverseInteger/ReverseInteger.py
@@ -1,10 +1,28 @@
-class Solution:   
+class Solution:
     def reverse(self, x: int) -> int:
-        if x > 2**31-1 or x < -2**31: return 0
-        revert = None
-        if x >= 0:
-            revert = int(str(x)[::-1])
-        else:
-            revert = int("-" + str(x)[1:][::-1])
-        if revert > 2**31-1 or revert < -2**31: return 0
-        return revert
+        negative = False
+        if x == 0:
+            return 0
+        if x < 0:
+            negative = True
+            x = abs(x)
+
+        digits = floor(math.log10(x))
+        mid = floor(digits / 2)
+        mask  = 10 ** digits
+        output = 0
+        for index in range(0, mid+1):
+            if index != digits-index:
+                output += int(x / mask) * (10**index)
+                output += int(x % 10) * (10**(digits-index))
+            else:
+                output += x * (10**index)
+            x = int(x % mask / 10)
+            mask = int(mask / 100)
+
+        if negative:
+            output = -output
+        if output > 2**31-1 or output < -2**31:
+            output = 0
+        return output
+        
